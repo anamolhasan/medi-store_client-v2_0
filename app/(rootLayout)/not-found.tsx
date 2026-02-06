ffdd261bb05f372230ae968a39a452dbbf8602bb
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-06 20:56:35 +0600

    not found page added

---
+++ app/(rootLayout)/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-3 min-h-screen">
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Link href={`/`}>
+                <Button className="cursor-pointer">Return Home</Button>
+            </Link>
+        </div>
+  )
+}
+
+export default NotFound
