9f0c16d483ec7bc73ba24cc7cd15f1b1f4e13174
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-09 13:23:06 +0600

    user service added

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,4 +1,5 @@
 import { env } from "@/env"
+import { UpdateUser } from "@/types/user.type";
 import { cookies } from "next/headers"
 
 
@@ -62,5 +63,153 @@ export const userService = {
                 error:{message:'Something went wrong', error}
             }
         }
+    },
+
+    getAllUsers: async () => {
+        try {
+            const cookieStore = await cookies();
+
+            const res = await fetch(`${API_URL}/user`, {
+                headers:{
+                    Cookie:cookieStore.toString(),
+                },
+                cache:'no-store',
+                next:{tags:['users']}
+            });
+
+            const session = await res.json()
+            if(session === null){
+                return {
+                    data:null,
+                    error:{message:"No session found", error:null}
+                }
+            }
+
+            return {data:session, error:null}
+        } catch (error) {
+            console.log(error);
+            return {
+                data:null,
+                error:{message:'Something went wrong', error}
+            }
+        }
+    },
+
+    getCustomerStats: async () => {
+        try {
+            const cookieStore = await cookies()
+            const res = await fetch(`${API_URL}/user/customer/status`, {
+                headers:{
+                    Cookie:cookieStore.toString(),
+                },
+                cache:'no-store'
+            })
+            const session = await res.json();
+            if(session === null){
+                return {
+                    data:null,
+                    error:{message:'No session found', error:null}
+                }
+            }
+            return {data:session, error:null}
+        } catch (error) {
+            console.log(error)
+            return {
+                data:null,
+                error:{message:"Something went wrong", error}
+            }
+        }
+    },
+
+    getSellerStats: async () => {
+        try {
+            const cookieStore = await cookies()
+
+            const res = await fetch(`$${API_URL}/user/seller/status`, {
+                headers:{
+                    Cookie:cookieStore.toString(),
+                },
+                cache:'no-store'
+            })
+
+            const session = await res.json()
+            if(session === null){
+                return {
+                    data:null,
+                    error:{message:'No session found', error:null}
+                }
+            }
+
+            return {data: session, error:null}
+        } catch (error) {
+            console.log(error)
+            return {
+                data:null,
+                error:{message:'Something went wrong', error}
+            }
+        }
+    },
+
+    getAdminStats: async () => {
+        try {
+            const cookieStore = await cookies();
+
+            const res = await fetch(`${API_URL}/user/admin/status`,{
+                headers:{
+                    Cookie:cookieStore.toString(),
+                },
+                cache:'no-store'
+            });
+            const session = await res.json();
+            if(session === null){
+                return {
+                    data:null,
+                    error:{message:'No session found', error:null}
+                }
+            }
+            return {data:session, error:null}
+        } catch (error) {
+            console.log(error);
+            return {
+                data:null,
+                error:{message:'Something went wrong', error}
+            }
+        }
+    },
+
+    updateUser: async (id:string, data:UpdateUser) => {
+          try {
+            const cookieStore = await cookies();
+            const res = await fetch(`$${API_URL}/user/${id}`, {
+                method:'PATCH',
+                headers:{
+                    "Content-Type":"application/json",
+                    Cookie:cookieStore.toString(),
+                },
+                body:JSON.stringify(data),
+                cache:'no-store'
+            })
+
+            if(!res.ok){
+                const errBody = await res.json().catch(()=> null);
+                return {
+                    data:null,
+                    error:{
+                        message:errBody?.message?? 'Failed to update user',
+                        error:errBody??null,
+                    }
+                }
+            }
+
+            const updated = await res.json()
+            return {data:updated, error:null};
+          } catch (error) {
+            console.log(null)
+            return {
+                data:null,
+                error:{message:'Something went wrong', error}
+            }
+          }
     }
 }
+
