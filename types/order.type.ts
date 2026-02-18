d3d31e8636c3f618fad193a23d54f84ae5dbab7b
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-18 23:06:44 +0600

    order type added

---
+++ types/order.type.ts
@@ -0,0 +1,56 @@
+import { Medicine } from "./medicine.type";
+import { User } from "./user.type";
+
+
+
+// enum
+export enum OrderStatus {
+    PLACED = 'PLACED',
+    PROCESSING = 'PROCESSING',
+    SHIPPED = 'SHIPPED',
+    DELIVERED = 'DELIVERED',
+    CANCELED = 'CANCELED',
+}
+
+// interfaces
+export interface Order {
+    id: string;
+    customerId:string;
+
+    status:OrderStatus;
+    shippingAddress:string;
+    totalAmount:number;
+
+    createdAt:Date;
+    updatedAt:Date;
+
+    // Relations
+    customer?:User;
+    items?:OrderItem
+}
+
+export interface OrderItem {
+    id:string;
+
+    orderId: string;
+    medicineId:string;
+
+    quantity:number;
+    price:number;
+
+    createdAt:Date;
+
+    //relations
+    order?:Order;
+    medicine?:Medicine;
+}
+
+export interface  CreateOrder {
+    customerId: string;
+    shippingAddress:string;
+    items:{
+        medicineId:string;
+        quantity:number;
+        price:number
+    }[]
+}
