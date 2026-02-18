5d52f26acec89c2e4c50b20bba777a89b30eecbc
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-18 23:23:15 +0600

    index and routes type added

---
+++ types/index.ts
@@ -0,0 +1,6 @@
+export type * from './user.type';
+export type * from './routes.type';
+export type * from './category.type';
+export type * from './medicine.type';
+export type * from './order.type';
+export type * from './review.type';
