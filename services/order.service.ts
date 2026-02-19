682ba3998b1da803c6f4eaa3444a5a29ea70da85
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-19 06:29:37 +0600

    order service

---
+++ services/order.service.ts
@@ -0,0 +1,183 @@
+import { env } from "@/env";
+import { CreateOrder, OrderStatus } from "@/types";
+import { cookies } from "next/headers";
+
+
+
+const API_URL = env.API_URL;
+
+export const orderService = {
+    createOrder: async (data:CreateOrder) => {
+         try {
+            const cookieStore = await cookies()
+
+            const res = await fetch(`${API_URL}/api/v1/order`, {
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                    Cookie: cookieStore.toString(),
+                },
+                body:JSON.stringify(data),
+                cache:'no-store',
+            })
+
+            if(!res.ok){
+                const errorBody = await res.json().catch(()=>null)
+                return {
+                    data:null,
+                    error:{
+                        message:errorBody?.message?? 'Failed to create order',
+                        error:errorBody ?? null,
+                    }
+                }
+            }
+
+            const updated = await res.json();
+            return {data:updated, error:null}
+         } catch (error) {
+            console.log(error);
+            return {
+                data:null,
+                error:{message:'Something went wrong', error},
+            }
+         }
+    },
+
+    getAllOrders: async () => {
+        try {
+            const cookieStore = await cookies();
+            const res = await fetch(`${API_URL}/api/v1/order`, {
+                method:'GET',
+                headers:{
+                    'Content-Type':'application/json',
+                    Cookie:cookieStore.toString(),
+                },
+                cache:'no-store',
+                next:{tags:['orders']},
+            })
+
+            if(!res.ok){
+                const errBody = await res.json().catch(()=> null);
+                return {
+                    data:null,
+                    error:{
+                        message:errBody?.message??'Failed to get order',
+                        error:errBody??null,
+                    }
+                }
+            }
+
+            const updated = await res.json();
+            return {date:updated, error:null}
+        } catch (error) {
+            console.log(error)
+            return {
+                data:null,
+                error:{message:'Something went wrong'}
+            }
+        }
+    },
+
+    updateOrderById: async (id: string, orderData: { status: OrderStatus }) => {
+        try {
+            const cookieStore = await cookies();
+            const res = await fetch(`${API_URL}/api/v1/order/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: cookieStore.toString(),
+                },
+                body: JSON.stringify(orderData),
+                cache: "no-store",
+            });
+
+            if (!res.ok) {
+                const errBody = await res.json().catch(() => null);
+                return {
+                    data: null,
+                    error: {
+                        message: errBody?.message ?? "Failed to update order",
+                        error: errBody ?? null,
+                    },
+                };
+            }
+
+            const updated = await res.json();
+            return { data: updated, error: null };
+        } catch (error) {
+            console.log(error);
+            return {
+                data: null,
+                error: { message: "Something went wrong", error },
+            };
+        }
+    },
+
+    deleteOrderById: async (id: string) => {
+        try {
+            const cookieStore = await cookies();
+            const res = await fetch(`${API_URL}/api/v1/order/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: cookieStore.toString(),
+                },
+                cache: "no-store",
+            });
+
+            if (!res.ok) {
+                const errBody = await res.json().catch(() => null);
+                return {
+                    data: null,
+                    error: {
+                        message: errBody?.message ?? "Failed to delete order",
+                        error: errBody ?? null,
+                    },
+                };
+            }
+
+            const updated = await res.json();
+            return { data: updated, error: null };
+        } catch (error) {
+            console.log(error);
+            return {
+                data: null,
+                error: { message: "Something went wrong", error },
+            };
+        }
+    },
+
+    getMyOrders: async (id: string) => {
+        try {
+            const cookieStore = await cookies();
+            const res = await fetch(`${API_URL}/api/v1/order/${id}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: cookieStore.toString(),
+                },
+                cache: "no-store",
+            });
+
+            if (!res.ok) {
+                const errBody = await res.json().catch(() => null);
+                return {
+                    data: null,
+                    error: {
+                        message: errBody?.message ?? "Failed to get order",
+                        error: errBody ?? null,
+                    },
+                };
+            }
+
+            const updated = await res.json();
+            return { data: updated, error: null };
+        } catch (error) {
+            console.log(error);
+            return {
+                data: null,
+                error: { message: "Something went wrong", error },
+            };
+        }
+    },
+}
